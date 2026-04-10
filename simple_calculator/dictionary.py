2a261086a997cfdb4b890bc5d1ece429b8272275
amantha2002 <amanthasandunaa@gmail.com>
2026-04-10 13:11:56 +0530

    create the simplest calculator using the dictionary

---
+++ simple_calculator/dictionary.py
@@ -0,0 +1,14 @@
+operator = input("Enter the operator (+ , / , * , -) : ")
+num1 = float(input("Enter the fiust numbet : "))
+num2 = float(input ("Enter the second number : "))
+
+operations = {
+    "+" : num1 + num2 ,
+    "-" : num1 - num2 , 
+    "*" : num1 * num2 , 
+    "/" : num1 / num2
+}
+ 
+result = operations.get(operator , "invalid operator")  # dictionary.get(key, default_value)
+print(result)                                           # key → the key you are looking for
+                                                        # default_value → what to return if the key is NOT found+
