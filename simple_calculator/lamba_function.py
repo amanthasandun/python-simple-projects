882ed639728ccce8c768d9b1421f0c11be3f042e
amantha2002 <amanthasandunaa@gmail.com>
2026-04-10 13:29:00 +0530

    create the simplest calculator using the lamda funtion '

---
+++ simple_calculator/lamba_function.py
@@ -0,0 +1,18 @@
+opertor = input("Enter the operation that what you want to do (+ , - , / , *) : ")
+num1 = float(input("Enter the first Number : ")) 
+num2 = float(input("Enter the second number : "))
+
+operations = {
+    "+" : lambda x,y : x + y ,
+    "-" : lambda x,y : x-y , 
+    "/" : lambda x,y : x/y , 
+    "*" : lambda x,y : x*y if y != 0 else "Error , devision by the zero"
+}
+
+func = operations.get(opertor)
+if func : 
+    result = func(num1 , num2)  # finally func is a lamda function .  so when we enter the values as in this way it aumatacally sign the parameters in the lamda funtion in the order 
+else :
+    result = "Invalid operator"
+
+print(result)
